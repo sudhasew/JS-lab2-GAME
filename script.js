75654533964b79886ba3af248fe4d721430f80f9
SudhaVallabhapurapu <89112048+sudhasew@users.noreply.github.com>
2022-09-05 12:21:58 -0500

    completed lab2

---
+++ script.js
@@ -0,0 +1,59 @@
+// Declare an arrow function named randomDamage that has no parameters
+// and returns a random integer between 1 and 10.
+
+const randomDamage = () => {
+  let random = Math.floor(Math.random() * 10);
+  return random;
+};
+
+// Declare an arrow function named chooseOption that has two parameters named opt1 and opt2.
+// Declares and initializes a variable named randNum to either a 0 or 1, randomly.
+// Returns opt1 if randNum is equal to 0 otherwise return opt2.
+// (Use a ternary operator)
+const chooseOption = (opt1, opt2) => {
+  let randNum = Math.floor(Math.random() * 2) ? opt1 : opt2;
+  return randNum;
+};
+
+function attackPlayer(health) {
+  return parseInt(health) - randomDamage();
+}
+
+const logHealth = (player, health) => {
+  console.log(`${player} health: ${health}`);
+};
+
+const logDeath = (winner, loser) => {
+  console.log(`${winner} defeated ${loser}`);
+};
+
+const isDead = (health) => {
+  if (health <= 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+function fight(player1, player2, player1Health, player2Health) {
+  while (true) {
+    let attacker = chooseOption(player1, player2);
+    if (attacker === player1) {
+      player2Health = attackPlayer(player2Health);
+      logHealth(player2, player2Health);
+      if (isDead(player2Health) === true) {
+        logDeath(player1, player2);
+        break;
+      } else {
+        player1Health = attackPlayer(player1Health);
+        logHealth(player1, player1Health);
+        if (isDead(player1Health) === true) {
+          logDeath(player2, player1);
+          break;
+        }
+      }
+    }
+  }
+}
+
+fight("Mitch Cuckovich", "Adam Hire", 100, 100);
